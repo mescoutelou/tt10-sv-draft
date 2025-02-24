98a55cdcd032e7685ac84ce54c2245c027c9b91f
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2025-02-24 20:44:09 +0100

    Try SV

--- src/top.sv
+++ src/top.sv
@@ -17,11 +17,26 @@ module tt_um_sv (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+//  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   logic _unused = &{ena, clk, rst_n, 1'b0};
+
+  always_ff @(posedge clock iff reset == 0 or posedge reset)
+  if (reset)
+    q <= 0;
+  else if (enable)
+    q++;
+
+  always_ff @(posedge clk) begin
+    if (~rst_n) begin
+      uo_out <= 'h00;         
+    end 
+    else begin
+      uo_out  = ui_in + uio_in
+    end
+  end
 
 endmodule
